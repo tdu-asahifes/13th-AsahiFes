eb1810f09f67ab62d0573f5ce1db9dde1f374aa7
yuta2005 <24fi052@ms.dendai.ac.jp>
2024-10-22 11:40:59 +0900

    up

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     let currentImageIndex = 0;
     let images = [];
 
     // モーダルを開くボタンのクリックイベントを設定
     document.querySelectorAll('.modal-button').forEach(button => {
-        button.addEventListener('click', function() {
+        button.addEventListener('click', function () {
             images = button.getAttribute('data-images').split(',');
             currentImageIndex = 0;
             const modalImage = document.getElementById('galleryImage');
@@ -13,7 +13,7 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     // モーダルウィンドウ内の画像クリックイベント
-    document.getElementById('galleryImage').addEventListener('click', function() {
+    document.getElementById('galleryImage').addEventListener('click', function () {
         if (images.length > 0) {
             currentImageIndex = (currentImageIndex + 1) % images.length;
             this.src = images[currentImageIndex];
@@ -21,17 +21,17 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 function countdown() {
-  const targetDate = new Date("November 2, 2024 00:00:00").getTime();
-  const now = new Date().getTime();
-  const timeRemaining = targetDate - now;
+    const targetDate = new Date("November 2, 2024 00:00:00").getTime();
+    const now = new Date().getTime();
+    const timeRemaining = targetDate - now;
 
-  const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
 
-  document.querySelector(".days").innerText = days;
+    document.querySelector(".days").innerText = days;
 
-  if (timeRemaining < 0) {
-      document.getElementById("countdown").innerHTML = "カウントダウン終了";
-  }
+    if (timeRemaining < 0) {
+        document.getElementById("countdown").innerHTML = "カウントダウン終了";
+    }
 }
 
 setInterval(countdown, 1000);
