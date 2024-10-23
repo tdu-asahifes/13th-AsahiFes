644baf16023dca0db9ac0da6d6820d82bab0e2d2
yuta2005 <24fi052@ms.dendai.ac.jp>
2024-10-23 20:15:09 +0900

    update modal

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let currentImageIndex = 0;
-    let images = [];
 
-    // モーダルを開くボタンのクリックイベントを設定
-    document.querySelectorAll('.modal-button').forEach(button => {
-        button.addEventListener('click', function () {
-            images = button.getAttribute('data-images').split(',');
-            currentImageIndex = 0;
-            const modalImage = document.getElementById('galleryImage');
-            modalImage.src = images[currentImageIndex]; // 最初の画像を表示
-        });
-    });
-
-    // モーダルウィンドウ内の画像クリックイベント
-    document.getElementById('galleryImage').addEventListener('click', function () {
-        if (images.length > 0) {
-            currentImageIndex = (currentImageIndex + 1) % images.length;
-            this.src = images[currentImageIndex];
-        }
-    });
-});
 function countdown() {
     const targetDate = new Date("November 2, 2024 00:00:00").getTime();
     const now = new Date().getTime();
@@ -38,20 +17,69 @@ setInterval(countdown, 1000);
 
 
 
-  document.querySelectorAll('.navbar-nav .nav-link').forEach(function (navLink) {
+document.querySelectorAll('.navbar-nav .nav-link').forEach(function (navLink) {
     navLink.addEventListener('click', function () {
-      var navbarCollapse = document.querySelector('.navbar-collapse');
-      var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
-        toggle: false
-      });
-      bsCollapse.hide(); // メニューを閉じる
+        var navbarCollapse = document.querySelector('.navbar-collapse');
+        var bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+            toggle: false
+        });
+        bsCollapse.hide(); // メニューを閉じる
     });
-  });
-
-  $('#exampleModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget); // クリックした画像
-    var modalImageSrc = button.data('modal-image'); // データ属性からモーダル画像のURLを取得
-    var modal = $(this);
-    modal.find('#modalImage').attr('src', modalImageSrc); // モーダル内の画像を設定
 });
 
+function modalOpen(kikakuId) {
+    //------------------------------------------------------------
+    //  モーダルウインドウ オープン
+    //------------------------------------------------------------
+    document.getElementById("modalArea1").className = "modalBg modalBgOpen";
+    changeText(kikakuId);
+    console.log(kikakuId);
+}
+
+function modalClose() {
+    //------------------------------------------------------------
+    //  モーダルウインドウ クローズ
+    //------------------------------------------------------------
+    document.getElementById("modalArea1").className = "modalBg modalBgClose";
+}
+
+// JavaScriptファイル (script.js) 内のコード
+
+// テキストを書き換える関数
+function changeText(kikakuId) {
+    // idが"target"の要素を取得
+    var targetElement = document.getElementById("target");
+    console.log(kikakuId);
+
+    // テキストを新しい内容に書き換える
+
+    if (kikakuId == "random") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"imeges/kikaku/furugisyousai.png\">";
+        console.log(kikakuId);
+    }
+    else if (kikakuId == "fureai") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"images/kikaku/fureaisyousai.png\">";
+        console.log(kikakuId);
+    }
+    else if (kikakuId == "1nen") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"images/kikaku/1nensyousai.png\">";
+
+        console.log(kikakuId);
+    }
+    else if (kikakuId == "kodomo") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"material/kikaku/kodomodetail.png\">";
+        console.log(kikakuId);
+    }
+    else if (kikakuId == "yatai") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"images/kikaku/yataisyousai.png\">";
+        console.log(kikakuId);
+    }
+    else if (kikakuId == "stamp") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"images/kikaku/stampsyousai.png\">";
+        console.log(kikakuId);
+    }
+    else if (kikakuId == "yatai") {
+        targetElement.innerHTML = "<img class=\"modalimg\" src=\"images/kikaku/yataisyousai.png\">";
+        console.log(kikakuId);
+    }
+}
