fa7ed4a8a5797952a2434ae9f22ed64c618917e9
YUKI <78786632+ykshio@users.noreply.github.com>
2024-10-29 00:13:01 +0900

    フォーマット

--- js/script.js
+++ js/script.js
@@ -1,24 +1,26 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   let currentImageIndex = 0;
   let images = [];
 
   // モーダルを開くボタンのクリックイベントを設定
-  document.querySelectorAll('.modal-button').forEach(button => {
-    button.addEventListener('click', function () {
-      images = button.getAttribute('data-images').split(',');
+  document.querySelectorAll(".modal-button").forEach((button) => {
+    button.addEventListener("click", function () {
+      images = button.getAttribute("data-images").split(",");
       currentImageIndex = 0;
-      const modalImage = document.getElementById('galleryImage');
+      const modalImage = document.getElementById("galleryImage");
       modalImage.src = images[currentImageIndex]; // 最初の画像を表示
     });
   });
 
   // モーダルウィンドウ内の画像クリックイベント
-  document.getElementById('galleryImage').addEventListener('click', function () {
-    if (images.length > 0) {
-      currentImageIndex = (currentImageIndex + 1) % images.length;
-      this.src = images[currentImageIndex];
-    }
-  });
+  document
+    .getElementById("galleryImage")
+    .addEventListener("click", function () {
+      if (images.length > 0) {
+        currentImageIndex = (currentImageIndex + 1) % images.length;
+        this.src = images[currentImageIndex];
+      }
+    });
 });
 function countdown() {
   const targetDate = new Date("November 2, 2024 00:00:00").getTime();
@@ -36,28 +38,23 @@ function countdown() {
 
 setInterval(countdown, 1000);
 
-
-
 // モーダル背景をクリックした時にモーダルを閉じる処理
-document.addEventListener('click', function (event) {
+document.addEventListener("click", function (event) {
   // モーダルの外側をクリックしたときのみ閉じる
-  var modal = document.getElementById('popupModal');
+  var modal = document.getElementById("popupModal");
   if (event.target === modal) {
     var bootstrapModal = bootstrap.Modal.getInstance(modal); // Bootstrapのモーダルインスタンスを取得
     bootstrapModal.hide(); // モーダルを閉じる
   }
 });
 
-
-
-
 function openPopup(element) {
   // data-popup-url属性からURLを取得
-  var url = element.getAttribute('data-popup-url');
+  var url = element.getAttribute("data-popup-url");
   // iframeに取得したURLを設定
-  document.getElementById('externalContent').src = url;
+  document.getElementById("externalContent").src = url;
 
   // モーダルを表示
-  var myModal = new bootstrap.Modal(document.getElementById('popupModal'));
+  var myModal = new bootstrap.Modal(document.getElementById("popupModal"));
   myModal.show();
 }
